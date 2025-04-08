94b958221a4e4eae7a8ca8caf97c25ba8d49374a
Khetani Jainam Anilbhai <24CS039@charusat.edu.in>
2025-04-08 11:39:14 +0530

    Create 8 - Stock buy and sell.cpp

---
+++ 8 - Stock buy and sell.cpp
@@ -0,0 +1,71 @@
+/*Stock Buy and Sell – Max one Transaction Allowed
+Difficulty: EasyAccuracy: 49.33%Submissions: 76K+Points: 2Average Time: 10m
+Given an array prices[] of length n, representing the prices of the stocks on different days. The task is to find the maximum profit possible by buying and selling the stocks on different days when at most one transaction is allowed. Here one transaction means 1 buy + 1 Sell. If it is not possible to make a profit then return 0.
+
+Note: Stock must be bought before being sold.
+
+Examples:
+
+Input: prices[] = [7, 10, 1, 3, 6, 9, 2]
+Output: 8
+Explanation: You can buy the stock on day 2 at price = 1 and sell it on day 5 at price = 9. Hence, the profit is 8.
+Input: prices[] = [7, 6, 4, 3, 1]
+Output: 0 
+Explanation: Here the prices are in decreasing order, hence if we buy any day then we cannot sell it at a greater price. Hence, the answer is 0.
+Input: prices[] = [1, 3, 6, 9, 11]
+Output: 10 
+Explanation: Since the array is sorted in increasing order, we can make maximum profit by buying at price[0] and selling at price[n-1].
+Constraint:
+1 <= prices.size()<= 105
+0 <= prices[i] <=104*/
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) {
+        // code here
+        int minP=INT_MAX, maxP=0;
+        for(int i=0;i<prices.size(); i++){
+            if(prices[i] < minP){
+                minP=prices[i];
+            }else if(prices[i] - minP>maxP){
+                maxP = prices[i] - minP;
+            }
+        }
+        return maxP;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> prices;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            prices.push_back(number);
+        }
+
+        Solution ob;
+        int ans = ob.maximumProfit(prices);
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
