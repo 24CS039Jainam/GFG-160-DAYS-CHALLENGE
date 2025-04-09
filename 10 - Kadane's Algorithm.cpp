d2aed301051a1bc2de75d526fc08ef04f81c902a
Khetani Jainam Anilbhai <24CS039@charusat.edu.in>
2025-04-09 07:38:56 +0530

    Create 10 - Kadane's Algorithm.cpp

---
+++ 10 - Kadane's Algorithm.cpp
@@ -0,0 +1,65 @@
+/*Kadane's Algorithm
+Difficulty: MediumAccuracy: 36.28%Submissions: 1.1MPoints: 4Average Time: 20m
+Given an integer array arr[]. You need to find the maximum sum of a subarray.
+
+Examples:
+
+Input: arr[] = [2, 3, -8, 7, -1, 2, 3]
+Output: 11
+Explanation: The subarray {7, -1, 2, 3} has the largest sum 11.
+Input: arr[] = [-2, -4]
+Output: -2
+Explanation: The subarray {-2} has the largest sum -2.
+Input: arr[] = [5, 4, 1, 7, 8]
+Output: 25
+Explanation: The subarray {5, 4, 1, 7, 8} has the largest sum 25.
+Constraints:
+1 ≤ arr.size() ≤ 105
+-109 ≤ arr[i] ≤ 104*/
+
+
+
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int maxSubarraySum(vector<int> &a) {
+        // Your code here
+        int maxS = a[0], curr = a[0];
+        for(int i=1; i<a.size(); i++){
+            curr=max(a[i],curr + a[i]);
+            maxS = max(maxS, curr);
+        }
+        return maxS;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To discard any leftover newline characters
+    while (t--)   // while testcases exist
+    {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.maxSubarraySum(arr) << endl << "~" << endl;
+    }
+}
+// } Driver Code Ends
